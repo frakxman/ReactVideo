b664455d020c0e6ce38e8bc5d7a9a451e70d25aa
Fabian Rivera <frakxman@hotmail.com>
2020-12-20 01:59:18 -0500

    Base proyect Handbook

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+
+const HelloWorld = () => <h1>Hello Frakx</h1>;
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWordl from './components/HelloWorld';
+
+ReactDOM.render(<HelloWordl />, document.getElementById("app"));
+
